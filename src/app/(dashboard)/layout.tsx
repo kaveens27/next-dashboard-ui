c79b411c75f2637bdfb89c0c00d54d10a0c4fc32
Kaveen <kaveens27@gmail.com>
2024-10-10 16:54:24 +0100

    add sidebar to dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <div></div>
+      <div className = 'h-screen flex'> {/* div for the whole page */}
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">{/* left hand side sidebar div - creates resonsiveness using tailwind css*/}
+          <Link href="/" className="flex items-center justify-center lg:justify-start gap-2"> {/* Creating the logo link to root page */}
+            <Image src="/logo.png" alt='logo' width={32} height={32}/> {/* Creating the logo image */}
+            <span className="hidden lg:block">SchoolTool</span> {/* Creating name to be displaed for larger screens and hides otherwise*/}
+          </Link>
+          <Menu/> {/* display the menu component on the sidebar */}
+        </div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">{/*Right side div*/}
+          r
+        </div>
+      </div>
     );
   }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,29 @@ const menuItems = [
     ],
   },
 ];
+
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className='mt-4 text-sm'> {/* This will iterative thought the MenuItem and create HTML for each index */}
+      {menuItems.map( i => (
+        <div className='flex flex-col gap-2' key={i.title}>
+          <span className='hidden lg:block text-gray-400 font-light my-4'>{i.title}</span> {/* Display the titles of the data ie Menu and Other */}
+          {i.items.map( item => ( 
+            <Link 
+              href={item.href} 
+              key={item.label} 
+              className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'
+            > 
+              <Image src={item.icon} alt={item.label} width={20} height={20} /> {/* Display image of each menu item */}
+              <span className='hidden lg:block'>{item.label}</span> {/* Display the title of each menu item */}
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
