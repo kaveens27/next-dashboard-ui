adb002bd70346f24fb350b52e8d0631224f5832e
Kaveen <kaveens27@gmail.com>
2024-10-16 23:16:59 +0100

    added user role dependancy

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { role } from '@/lib/data';
 
 const menuItems = [
   {
@@ -121,17 +122,23 @@ const Menu = () => {
     <div className='mt-4 text-sm'> {/* This will iterative thought the MenuItem and create HTML for each index */}
       {menuItems.map( i => (
         <div className='flex flex-col gap-2' key={i.title}>
-          <span className='hidden lg:block text-gray-400 font-light my-4'>{i.title}</span> {/* Display the titles of the data ie Menu and Other */}
-          {i.items.map( item => ( 
-            <Link 
-              href={item.href} 
-              key={item.label} 
-              className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2'
-            > 
-              <Image src={item.icon} alt={item.label} width={20} height={20} /> {/* Display image of each menu item */}
-              <span className='hidden lg:block'>{item.label}</span> {/* Display the title of each menu item */}
-            </Link>
-          ))}
+          <span className='hidden lg:block text-gray-400 font-light my-4'>
+            {i.title}
+          </span> {/* Display the titles of the data ie Menu and Other */}
+          {i.items.map((item) => {
+            if(item.visible.includes(role)){
+              return ( 
+                <Link 
+                  href={item.href} 
+                  key={item.label} 
+                  className='flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-ImportedPurpleLight'
+                > 
+                  <Image src={item.icon} alt={item.label} width={20} height={20} /> {/* Display image of each menu item */}
+                  <span className='hidden lg:block'>{item.label}</span> {/* Display the title of each menu item */}
+                </Link>
+              )
+            }
+          })}
         </div>
       ))}
     </div>
