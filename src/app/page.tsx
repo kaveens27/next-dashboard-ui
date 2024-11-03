c66e81b75e40b164669c20a32aa0b1684a2c0290
Kaveen <kaveens27@gmail.com>
2024-11-03 19:29:31 +0000

    added simple adminpage nav button to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className=''>
+      <Link href="/admin">
+        <button className="bg-blue-500 p-4 rounded-md text-xl font-semibold text-white m-4">Admin Page</button>
+      </Link>
+    </div>
   )
 }
 
